61912d8a0071f42c80810ac8298b7f760566bd3c
Minh Hiếu <gaconmvp2312@gmail.com>
2024-10-14 14:01:54 +0700

    init gateway

---
+++ api/gateway/src/main/java/com/universitylifeenhancement/gateway/config/CorsConfig.java
@@ -0,0 +1,32 @@
+/*
+ *  CorsConfifg
+ *  @author: Minhhieuano
+ *  @created 10/14/2024 1:42 PM
+ * */
+
+
+package com.universitylifeenhancement.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
+    }
+}
